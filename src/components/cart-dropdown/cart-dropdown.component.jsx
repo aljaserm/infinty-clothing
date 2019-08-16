c024687f99c75d1e25135f04b51bedb9681e2999
Mohammed alJaser <aljaserm7@gmail.com>
2019-08-15 20:45:22 -0400

    Add item count in cart icon. As well as cart selector to not rerender the cart if not related state changed

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.components';
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.components';
+import {selectCartItems} from '../../redux/cart/cart.selectors.js';
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
@@ -13,10 +14,10 @@ const CartDropdown = ({cartItems}) => (
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-)
+);
 
-const mapStateToProps = ({cart: { cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems:selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
