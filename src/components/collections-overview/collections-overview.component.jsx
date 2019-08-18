52eb419367789971d66baacf7594b1c3bfc5e42b
Mohammed alJaser <aljaserm7@gmail.com>
2019-08-18 18:26:05 -0400

    Added more shop sections

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {selectCollections} from '../../redux/shop/shop.selector.js';
+import {selectCollcetionForPreview} from '../../redux/shop/shop.selector';
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview from "../../components/collection-preview/collection-preview.components";
 import './collections-overview.styles.scss';
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) =>(
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections:selectCollections
+    collections:selectCollcetionForPreview
 });
 
 export default  connect(mapStateToProps)(CollectionsOverview);
