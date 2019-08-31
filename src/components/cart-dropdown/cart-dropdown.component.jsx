680ba605b6852a4119b16eb657a59add98a9cf0c
Mohammed alJaser <aljaserm7@gmail.com>
2019-08-31 11:18:18 -0400

    Used styled-components on cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
-import CustomButton from '../custom-button/custom-button.components';
-import './cart-dropdown.styles.scss';
+
+// import './cart-dropdown.styles.scss';
+import {CartDropDownContainer, CartItemContainer, EmptyMessageContainer,CartDropdownButton} from './cart-dropdown.styles';
 import CartItem from '../cart-item/cart-item.components';
 import {selectCartItems} from '../../redux/cart/cart.selectors.js';
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 const CartDropdown = ({cartItems, history, dispatch}) => (
-    <div className='cart-dropdown'>
-    <div className='cart-items'>
+    <CartDropDownContainer>
+    <CartItemContainer>
     {
         cartItems.length ?(
         cartItems.map(cartItem=> (
             <CartItem key={cartItem.id} item={cartItem} /> 
             ))
         ):(
-            <span className='empty-message'> Your cart is empty</span>
+            <EmptyMessageContainer> Your cart is empty</EmptyMessageContainer>
         )
     }
-    </div>
-    <CustomButton onClick={()=> {
+    </CartItemContainer>
+    <CartDropdownButton onClick={()=> {
         history.push('/checkout');
         dispatch(toggleCartHidden())
     }}>
-            GO TO CHECKOUT</CustomButton>
-    </div>
+            GO TO CHECKOUT</CartDropdownButton>
+    </CartDropDownContainer>
 );
 
 const mapStateToProps = createStructuredSelector ({
--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -16,8 +16,6 @@ const invertedButonStyle= css`
 background-color: white;
 color: dodgerblue;
 border: 1px solid dodgerblue;
-
-
 &:hover{
 background-color: dodgerblue;
 color: white;
@@ -51,7 +49,6 @@ letter-spacing: 0.5px;
 line-height: 50px;
 padding: 0 35px 0 35px;
 font-size: 15px;
-
 text-transform: uppercase;
 font-family: 'Open Sans Condensed';
 font-weight: bolder;
