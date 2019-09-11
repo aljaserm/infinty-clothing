c2533a63aa917b934e4750391b9db19bdc9e97c3
Mohammed alJaser <aljaserm7@gmail.com>
2019-09-10 21:26:25 -0400

    seprating our app to clint folder, added express server to handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
-const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
-}
+
 
 const  StripeCheckoutButton = ({ price })=>{
     const priceForStripe = price*100;
     const publishablekey= 'pk_test_n13Wdw21W2mUTqUxQA2pZQxi';
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('Payment failed, please use the provided credit card')
+        })
+    };
     return (
         <StripeCheckout 
         label='Pay Now'
