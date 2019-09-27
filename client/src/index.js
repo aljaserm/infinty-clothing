882a2d9c1b84cc444fe5caba4c7692fa4941fdd5
Mohammed alJaser <aljaserm7@gmail.com>
2019-09-26 20:37:42 -0400

    Convert To PWA

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import {store, persistor } from './redux/sotre';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 
@@ -18,3 +19,5 @@ ReactDOM.render(
     </Provider>,
      document.getElementById('root')
     );
+
+    serviceWorker.register();
